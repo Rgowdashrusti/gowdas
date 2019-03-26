4182f236c7c1e83e7cb39f1be9659f9c4fbbf989
H R <shrusti.h.r@accenture.com>
2019-03-26 14:00:57 +0530

    message

---
+++ funct.py
@@ -0,0 +1,18 @@
+print(1,2,3,4,sep='*')
+
+print('I love {1} and {0}'.format('bread','butter'))
+
+print('Hello {name}, {greeting}'
+      .format(greeting='Goodmorning', name='John'))
+
+c=eval('2+3')
+print(c)
+
+import math
+print(math.pi)
+
+from math import pi
+print(pi)
+
+import sys
+print(sys.path)
